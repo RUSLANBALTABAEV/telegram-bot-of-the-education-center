caab5abb0635e75c0019078797a19019566ea042
RUSLANBALTABAEV <ruslanotoriuos@gmail.com>
2025-09-07 16:46:37 +0500

    Fixed minor bugs

--- handlers/start.py
+++ handlers/start.py
@@ -8,6 +8,6 @@ start_router = Router()
 @start_router.message(Command("start"))
 async def cmd_start(message: types.Message):
     await message.answer(
-        "👋 Привет! Добро пожаловать!\nВыберите действие:",
+        "👋 Здравствуйте! Добро пожаловать!\nВыберите действие:",
         reply_markup=main_menu(message.from_user.id)
     )
